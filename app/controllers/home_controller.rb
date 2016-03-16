e6c9bedf1221cc846632188171636c40f501739f
HieuNM6 <hieunmce@gmail.com>
2016-03-17 00:21:37 +0700

    Add unread_message counter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+    if logged_in?
+      @messages = current_user.messages_received
+      @notifications = current_user.notifications
+      @unread_messages_count ||= unread_message @notifications
+    end
   end
+  private
+    def unread_message notifications
+      counter = 0
+      notifications.each do |n|
+        unless n.read
+         counter += 1  
+        end
+      end
+      counter
+    end
 end
