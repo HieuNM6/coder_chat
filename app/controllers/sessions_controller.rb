619307f39a231d5342480ded6419e44bcb04f3b3
HieuNM6 <hieunmce@gmail.com>
2016-03-16 19:44:42 +0700

    Add feature login\logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email:params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+        flash[:success] = "Login successful"
+        session[:user_id] = @user.id
+        redirect_to root_path
+    else
+        flash.now[:error] = "Login unsuccessful"
+        render 'new'
+    end
+  end
+
+  def destroy
+    flash[:success] = "Logout successful"
+    session[:user_id] = nil 
+    redirect_to root_path  
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  get 'login' => 'sessions#new'
+  post 'new_session' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
