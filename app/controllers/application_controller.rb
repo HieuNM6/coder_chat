b97998a987e810f113f948bdf418e4927337396e
HieuNM6 <hieunmce@gmail.com>
2016-03-20 02:33:29 +0700

    Update fix heroku can logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
 
 
   def current_user
-    current_user ||= User.find_by_id(session[:user_id])
+    if session[:user_id].present?
+      current_user ||= User.find_by_id(session[:user_id])
+    end
   end
 
   def logged_in?
