61016319311297fd163c2257518a4d7bfed8fb7a
HieuNM6 <hieunmce@gmail.com>
2016-03-20 02:34:56 +0700

    asdfsadfsadfa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,7 @@ class ApplicationController < ActionController::Base
 
 
   def current_user
-    if session[:user_id].present?
       current_user ||= User.find_by_id(session[:user_id])
-    end
   end
 
   def logged_in?
